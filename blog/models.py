8e7e2fccfb1cded5fec3305647e31e3f76aee407
nahu <nahuboutet@gmail.com>
2018-10-16 23:07:00 -0300

    tercer commit

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,20 @@
+''' Imports '''
 from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
+    "Clase Post"
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+        default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
 
     def publish(self):
+        "Publica post"
         self.published_date = timezone.now()
         self.save()
 
